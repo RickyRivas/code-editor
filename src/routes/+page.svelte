5cdbd8f62c25ac2118a2fdccdd9378befb9562cc
Ricky Rivas <rickyrivas918@gmail.com>
2024-10-12 02:09:50 -0500

    working. edit code snippet in real time, in browser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,104 @@
 <script>
   // stores
-  import { business } from "$lib/config"
   import Prism from "prismjs"
+  import { EditorView, basicSetup } from "codemirror"
+  import { EditorState, StateEffect, StateField } from "@codemirror/state"
+  import { javascript } from "@codemirror/lang-javascript"
+  import { html } from "@codemirror/lang-html"
+  import { css } from "@codemirror/lang-css"
   import beautify from "js-beautify"
-
-  // Import additional languages
-  import "prismjs/components/prism-javascript"
-  import "prismjs/components/prism-css"
-  import "prismjs/components/prism-markup"
   import { onMount } from "svelte"
+  import { oneDark } from "@codemirror/theme-one-dark"
 
   let code = `@media only screen and (max-width: 767px) {  margin: 0 0 2em 0;  }  @media only screen and (min-width: 768px) {  margin: 0 2em 0 0;  }`
   let language = "css"
-  let highlighted = ""
 
+  let editor
+  let editorElement
   let isEditing = false
-  let editableCode = code
 
-  function formatAndHighlight() {
-    let formattedCode = code
+  const toggleEditableEffect = StateEffect.define()
 
-    // Format the code based on the language
-    if (language === "css") {
-      formattedCode = beautify.css(code)
-    } else if (language === "html") {
-      formattedCode = beautify.html(code)
-    } else if (language === "javascript" || language === "js") {
-      formattedCode = beautify.js(code)
+  const editableExtension = StateField.define({
+    create: () => false,
+    update(value, tr) {
+      for (let e of tr.effects) if (e.is(toggleEditableEffect)) value = e.value
+      return value
+    },
+    provide: (f) => EditorView.editable.from(f),
+  })
+
+  function getLangSupport(lang) {
+    switch (lang) {
+      case "javascript":
+      case "js":
+        return javascript()
+      case "css":
+        return css()
+      case "html":
+        return html()
+      default:
+        return javascript()
     }
+  }
 
-    highlighted = Prism.highlight(formattedCode, Prism.languages[language], language)
+  function formatCode(codeToFormat, lang) {
+    switch (lang) {
+      case "css":
+        return beautify.css(codeToFormat)
+      case "html":
+        return beautify.html(codeToFormat)
+      case "javascript":
+      case "js":
+        return beautify.js(codeToFormat)
+      default:
+        return codeToFormat
+    }
   }
 
   function toggleEdit() {
     isEditing = !isEditing
+
+    // editor
+    editor.dispatch({
+      effects: toggleEditableEffect.of(isEditing),
+    })
+
+    // when saving changes, update code snippet
+    // TODO: save to db here
     if (!isEditing) {
-      code = editableCode
-      formatAndHighlight()
+      const updatedCode = editor.state.doc.toString()
+      code = updatedCode
+      console.log("updated code:", code)
     }
   }
 
   onMount(() => {
-    formatAndHighlight()
+    const langSupport = getLangSupport(language)
+    const formattedCode = formatCode(code, language)
+
+    const state = EditorState.create({
+      doc: formattedCode,
+      extensions: [basicSetup, langSupport, oneDark, EditorView.lineWrapping, editableExtension],
+    })
+
+    editor = new EditorView({
+      state,
+      parent: editorElement,
+    })
+
+    return () => {
+      editor.destroy()
+    }
   })
+
+  $: console.log("isEditing", isEditing)
 </script>
 
 <main>
   <section id="hero">
-    <div class="code-editor">
-      {#if isEditing}
-        <textarea bind:value={editableCode} rows="10" cols="50" id=""></textarea>
-        <button on:click={toggleEdit}>save changes</button>
-      {:else}
-        <pre><code class="language-{language}">{@html highlighted}</code></pre>
-        <button on:click={toggleEdit}>Edit</button>
-      {/if}
-    </div>
+    <div class="code-mirror-el" bind:this={editorElement}></div>
+    <button on:click={toggleEdit}>{isEditing ? "Save Changes" : "Edit"}</button>
   </section>
 </main>
 
