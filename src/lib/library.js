900aa63aff0e2d33c2612244dbdee53891d17eed
Ricky Rivas <rickyrivas918@gmail.com>
2024-10-12 01:00:46 -0500

    simple highlighting, formatting string snippets

---
+++ src/lib/library.js
@@ -0,0 +1,12 @@
+export const library = [
+    {
+        category: 'cards',
+        data: [
+            {
+                title: '3 cards',
+                snippet: ``,
+                lang: 'css'
+            },
+        ]
+    },
+]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,83 @@
 <script>
-  // components
-  import SEO from "./../lib/components/SEO.svelte"
-
   // stores
   import { business } from "$lib/config"
+  import Prism from "prismjs"
+  import beautify from "js-beautify"
+
+  // Import additional languages
+  import "prismjs/components/prism-javascript"
+  import "prismjs/components/prism-css"
+  import "prismjs/components/prism-markup"
   import { onMount } from "svelte"
+
+  let code = `@media only screen and (max-width: 767px) {  margin: 0 0 2em 0;  }  @media only screen and (min-width: 768px) {  margin: 0 2em 0 0;  }`
+  let language = "css"
+  let highlighted = ""
+
+  let isEditing = false
+  let editableCode = code
+
+  function formatAndHighlight() {
+    let formattedCode = code
+
+    // Format the code based on the language
+    if (language === "css") {
+      formattedCode = beautify.css(code)
+    } else if (language === "html") {
+      formattedCode = beautify.html(code)
+    } else if (language === "javascript" || language === "js") {
+      formattedCode = beautify.js(code)
+    }
+
+    highlighted = Prism.highlight(formattedCode, Prism.languages[language], language)
+  }
+
+  function toggleEdit() {
+    isEditing = !isEditing
+    if (!isEditing) {
+      code = editableCode
+      formatAndHighlight()
+    }
+  }
+
+  onMount(() => {
+    formatAndHighlight()
+  })
 </script>
 
-<SEO
-  title="{business.name} | {business.industry} | {business.cityAndState}"
-  description={business.homepage.metaDescription}
-  canonical={business.canonical}
-  siteName={business.name}
-  imageURL={business.ogImage}
-  index={true}
-  twitter={true}
-  openGraph={true}
-  schemaOrg={false}
-  logo=""
-  author=""
-  name="" />
-
 <main>
-  <section id="hero"></section>
+  <section id="hero">
+    <div class="code-editor">
+      {#if isEditing}
+        <textarea bind:value={editableCode} rows="10" cols="50" id=""></textarea>
+        <button on:click={toggleEdit}>save changes</button>
+      {:else}
+        <pre><code class="language-{language}">{@html highlighted}</code></pre>
+        <button on:click={toggleEdit}>Edit</button>
+      {/if}
+    </div>
+  </section>
 </main>
+
+<style lang="less">
+  .code-editor {
+    display: block;
+    overflow: auto;
+    height: 100%;
+    height: (400/20em);
+    resize: vertical;
+    padding: 1em;
+    background-color: #2d2d2d;
+    width: 90%;
+    max-width: (600/20em);
+    display: block;
+    margin-inline: auto;
+
+    pre {
+      font-size: 14px;
+      font-family: inherit;
+    }
+  }
+
+  @import url("$styles/highlight.less");
+</style>
